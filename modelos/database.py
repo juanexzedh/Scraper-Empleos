1d61c41968d7932c4d605067d8ef540f11286ad4
Juanex <juanexzedh@gmail.com>
2026-06-19 23:24:28 -0500

    Se crean las funciones que realizan consultas SQL para el desarrollo posterior del dashboard

--- modelos/database.py
+++ modelos/database.py
@@ -1,5 +1,4 @@
-import sqlite3
-import os
+import sqlite3, os
 
 def conectar_db():
     try:
@@ -78,7 +77,7 @@ def insertar_ofertas(lista_ofertas):
     print(cursor.fetchall())
     conexion.close()
 
-
+#NUMERO DE OFERTAS
 def obtener_total_ofertas():
     conexion = sqlite3.connect("data/empleos.db")
     cursor = conexion.cursor()
@@ -89,22 +88,87 @@ def obtener_total_ofertas():
     conexion.close()
     return total
 
+#EMPRESAS CON MAS OFERTAS
 def obtener_empresas_top():
     conexion = sqlite3.connect("data/empleos.db")
     cursor = conexion.cursor()
 
-    cursor.execute("SELECT COUNT(*) FROM empleos")
-    total = cursor.fetchone()[0]
+    cursor.execute("""
+        SELECT empresa, COUNT(*)
+        FROM empleos
+        GROUP BY empresa
+        ORDER BY COUNT(*) DESC
+        LIMIT 10
+    """)
+
+    resultado = cursor.fetchall()
 
     conexion.close()
-    return total
+    return resultado
 
+#CIUDADES CON MAS OFERTAS
 def obtener_ciudades_top():
+    conexion = sqlite3.connect("data/empleos.db")
+    cursor = conexion.cursor()
 
+    cursor.execute("""
+        SELECT ciudad, COUNT(*)
+        FROM empleos
+        GROUP BY ciudad
+        ORDER BY COUNT(*) DESC
+        LIMIT 10
+    """)
+    resultado = cursor.fetchall()
+    conexion.close()
+    return resultado
 
+#SALARIO PROMEDIO
 def obtener_salario_promedio():
+    conexion = sqlite3.connect("data/empleos.db")
+    cursor = conexion.cursor()
 
+    cursor.execute("SELECT AVG(salario) AS promedio FROM empleos")
+    promedio = cursor.fetchone()
 
+    conexion.close()
+    return promedio
+
+#BUSCADOR POR KEYWORD
 def buscar_por_palabra_clave():
+    conexion = sqlite3.connect("data/empleos.db")
+    cursor = conexion.cursor()
+
+    TECNOLOGIAS = [
+        "Python", "Java", "JavaScript", "SQL", "React",
+        "Angular", "Docker", "AWS", "Git",".NET", 
+        "C#", "Linux", "HTML", "CSS", "PHP",
+        "Node.js", "IA", "Docker", "Tailwind", "ApiRest",
+        "Json", "Html5", "C#", "C++", "SOAP",
+        "RESTFUL", "Windows", "Angular", "Node.js", "Express",
+        "TypeScript", "Ruby", "PHP", "Kotlin", 
+        "MySQL", "Oracle", "PostgreSQL", "Azure", "Google Cloud",
+        "API", "Laravel", "Flask", "Django", "FastAPI", "Spring",
+        "XML", "Data Science", "Big Data", "Power BI"
+    ]
+
+    cursor.execute("""
+        SELECT descripcion, requerimientos, palabras_clave
+        FROM empleos
+    """)
+
+    ofertas = cursor.fetchall()
+    conteo = {}
+
+    for tecnologia in TECNOLOGIAS:
+        conteo[tecnologia] = 0 #Se le establece un contador a cada tecnologia o palabra clave
+
+    for descripcion, requerimientos, palabras_clave in ofertas:
+        texto = f"{descripcion} {requerimientos} {palabras_clave}".lower() #convierte todo a un texto unico para hacerlo mas facil de analizar
+        for tecnologia in TECNOLOGIAS: #por cada palabra clave en la lista de tecnologias
+            if tecnologia.lower() in texto: #si la tecnologia (en miniscula) esta en el texto, le añade uno al contador de esa tecnologia
+                conteo[tecnologia] += 1
+
+    conexion.close()
+    return conteo
 
 
