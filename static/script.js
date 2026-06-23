4f0329057777e1fb8889d7d24400a5097139632c
Juanex <juanexzedh@gmail.com>
2026-06-22 23:06:03 -0500

    Se realizo la grafica de tecnologias a traves de un archivo js junto con el establecimiento de las variables en el index.html

---
+++ static/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const ctx = document.getElementById('miGrafica').getContext('2d');
+    const datosX = tecnologias;
+    const datosY = cantidades;
+
+    const miGrafica = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: datosX,
+            datasets: [{
+                label: 'Tecnologías más solicitadas ($)',
+                data: datosY,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderWidth: 2,
+                tension: 0.3
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+});
